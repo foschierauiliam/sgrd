0f2ab20f3484b2a142d988ee350dd55fd975be29
foschierauiliam <48731781+foschierauiliam@users.noreply.github.com>
2021-11-22 14:46:17 -0300

    Update conn.php
    Alteração das configurações de conexão do database visto a mudanças na plataforma Heroku

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
 <?php
-	$str_conexao = "host=ec2-184-72-238-22.compute-1.amazonaws.com dbname=d9e8avgv2d38gv user=kcawpzlaalfrjt password=5819dd1f663cf8ce23971ac81056b46e7061bec6d007ba131d8292077a036387";
+	$str_conexao = "host=    ec2-54-225-187-177.compute-1.amazonaws.com dbname=d9e8avgv2d38gv user=d1ieaa0m3khvv1 password=666baafe7c912f1684d1d72602a086c6ef986042c62ffa8477ba9a94809882d1";
 	$conexao = pg_connect($str_conexao) or die ("Não foi possível conectar-se ao banco de dados PostgreSQL. Erro: ".pg_last_error());
 ?>
